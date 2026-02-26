6b5da7792ba5de81143046dee9e7c9e7492cbd46
ysaf-dev <nycfrigillana2024@plm.edu.ph>
2026-02-26 19:42:47 +0800

    added core window

--- ui/main.py
+++ ui/main.py
@@ -8,6 +8,7 @@ from kivy.uix.spinner import Spinner
 from kivy.uix. textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.core.window import Window
 
 from paradigms import procedural
 
